616f0a1ccba128c521b3743f723a2730aff3d727
bttur <btturet@outlook.com>
2021-05-04 18:55:34 -0500

    Changes by hughes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import pygame, sys
-from pygame.locals import *
+import pygame
+import sys
+from pygame.locals import *  # TODO: change to specific imports
+from pathlib import Path
 
 # Initialize program
 pygame.init()
@@ -7,6 +9,11 @@ pygame.init()
 # Assign FPS a value
 FPS = 30
 FramePerSec = pygame.time.Clock()
+
+# Root path to our assets
+ASSETS_PATH = Path("Assets")
+background = pygame.image.load(ASSETS_PATH / "AnimatedStreet.png")
+player_image = pygame.image.load(ASSETS_PATH / "Player.png")
 
 # Setting up color objects
 BLUE = (0, 0, 255)
@@ -16,18 +23,31 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 # Setup a 300x300 pixel display with caption
-DISPLAYSURF = pygame.display.set_mode((300, 300))
-DISPLAYSURF.fill(WHITE)
-pygame.display.set_caption("Example")
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+display_surface.fill(WHITE)
+pygame.display.set_caption("pycar-racing")
 
-# Creating Lines and Shapes
-pygame.draw.line(DISPLAYSURF, BLUE, (150, 130), (130, 170))
-pygame.draw.line(DISPLAYSURF, BLUE, (150, 130), (170, 170))
-pygame.draw.line(DISPLAYSURF, GREEN, (130, 170), (170, 170))
-pygame.draw.circle(DISPLAYSURF, BLACK, (100, 50), 30)
-pygame.draw.circle(DISPLAYSURF, BLACK, (200, 50), 30)
-pygame.draw.rect(DISPLAYSURF, RED, (100, 200, 100, 50), 2)
-pygame.draw.rect(DISPLAYSURF, BLACK, (110, 260, 80, 5))
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(ASSETS_PATH / "Player.png")
+        self.surf = pygame.Surface((40, 75))
+        self.rect = self.surf.get_rect(center=(160, 520))
+
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH and pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5,0)
+
+player = Player()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 # Beginning Game Loop
 while True:
@@ -36,5 +56,10 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+    display_surface.blit(background, (0, 0))
+    for entity in all_sprites:
+        display_surface.blit(entity.image, entity.rect)
+        entity.move()
 
     FramePerSec.tick(FPS)
